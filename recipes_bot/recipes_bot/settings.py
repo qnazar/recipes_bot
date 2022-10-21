66143480dda5fba4006610284227fa3c40366b36
Nazar Klypych <xxxnazarkoxxx@gmail.com>
2022-10-21 15:26:16 +0300

    handling files

--- recipes_bot/recipes_bot/settings.py
+++ recipes_bot/recipes_bot/settings.py
@@ -131,8 +131,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
